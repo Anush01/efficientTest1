6542b79b6ccb3a929a8778647d1174c1f4a02bc1
anush <02.anush@gmail.com>
2021-09-10 09:16:34 +0000

    cool

---
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import retrofit2.Retrofit;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Retrofit rf = new Retrofit.Builder().build();
+
+
+
+    }
+}
